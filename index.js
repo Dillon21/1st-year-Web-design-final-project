f13bfa61c457450da7f9901b3d4565317bf8ff88
DILLON SCOTT (1604465) <d.scott15@rgu.ac.uk>
2022-03-01 15:37:24 +0000

    added server js for test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http')
 const fs = require('fs')
+const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
 res.writeHead(200, { 'content-type': 'text/html' })
 fs.createReadStream('index1.html').pipe(res)
-})
-server.listen(process.env.PORT || 3000)
+});
+server.listen(process.env.PORT || 8080)
 console.log('Server running at http://127.0.0.1:3000/');
